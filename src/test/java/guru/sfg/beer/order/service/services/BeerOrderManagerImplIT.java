df6fe73a129d7503443bda9442ecb67c001b9e5c
Zun <zunsamuel97@gmail.com>
2020-09-08 11:19:08 -0500

    step 244

--- src/test/java/guru/sfg/beer/order/service/services/BeerOrderManagerImplIT.java
+++ src/test/java/guru/sfg/beer/order/service/services/BeerOrderManagerImplIT.java
@@ -30,7 +30,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.awaitility.Awaitility.await;
 import static org.jgroups.util.Util.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by jt on 2/14/20.
@@ -152,6 +152,42 @@ public class BeerOrderManagerImplIT {
         BeerOrder pickedUpOrder = beerOrderRepository.findById(savedBeerOrder.getId()).get();
 
         assertEquals(BeerOrderStatusEnum.PICKED_UP, pickedUpOrder.getOrderStatus());
+    }
+
+    @Test
+    void testAllocationFailure() throws JsonProcessingException {
+        BeerDto beerDto = BeerDto.builder().id(beerId).upc("12345").build();
+
+        wireMockServer.stubFor(get(BeerServiceImpl.BEER_UPC_PATH_V1 + "12345")
+                .willReturn(okJson(objectMapper.writeValueAsString(beerDto))));
+
+        BeerOrder beerOrder = createBeerOrder();
+        beerOrder.setCustomerRef("fail-allocation");
+
+        BeerOrder savedBeerOrder = beerOrderManager.newBeerOrder(beerOrder);
+
+        await().untilAsserted(() -> {
+            BeerOrder foundOrder = beerOrderRepository.findById(beerOrder.getId()).get();
+            assertEquals(BeerOrderStatusEnum.ALLOCATION_EXCEPTION, foundOrder.getOrderStatus());
+        });
+    }
+
+    @Test
+    void testPartialAllocation() throws JsonProcessingException {
+        BeerDto beerDto = BeerDto.builder().id(beerId).upc("12345").build();
+
+        wireMockServer.stubFor(get(BeerServiceImpl.BEER_UPC_PATH_V1 + "12345")
+                .willReturn(okJson(objectMapper.writeValueAsString(beerDto))));
+
+        BeerOrder beerOrder = createBeerOrder();
+        beerOrder.setCustomerRef("partial-allocation");
+
+        BeerOrder savedBeerOrder = beerOrderManager.newBeerOrder(beerOrder);
+
+        await().untilAsserted(() -> {
+            BeerOrder foundOrder = beerOrderRepository.findById(beerOrder.getId()).get();
+            assertEquals(BeerOrderStatusEnum.PENDING_INVENTORY, foundOrder.getOrderStatus());
+        });
     }
 
     public BeerOrder createBeerOrder(){
--- src/test/java/guru/sfg/beer/order/service/services/testcomponets/BeerOrderAllocationListener.java
+++ src/test/java/guru/sfg/beer/order/service/services/testcomponets/BeerOrderAllocationListener.java
@@ -1,6 +1,7 @@
 package guru.sfg.beer.order.service.services.testcomponets;
 
 import guru.sfg.beer.order.service.config.JmsConfig;
+import guru.sfg.brewery.model.BeerOrderDto;
 import guru.sfg.brewery.model.events.AllocateOrderRequest;
 import guru.sfg.brewery.model.events.AllocateOrderResult;
 import lombok.RequiredArgsConstructor;
@@ -24,15 +25,28 @@ public class BeerOrderAllocationListener {
     public void listen(Message msg){
         AllocateOrderRequest request = (AllocateOrderRequest) msg.getPayload();
 
-        request.getBeerOrderDto().getBeerOrderLines().forEach(beerOrderLineDto -> {
-            beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity());
+        boolean pendingInventory = false;
+        boolean allocationError = false;
+
+        BeerOrderDto beerOrderDto = request.getBeerOrderDto();
+        String customerRef = beerOrderDto.getCustomerRef();
+
+        if(customerRef != null) {
+            pendingInventory = customerRef.equals("partial-allocation");
+            allocationError = customerRef.equals("fail-allocation");
+        }
+
+        boolean finalPendingInventory = pendingInventory;
+        beerOrderDto.getBeerOrderLines().forEach(beerOrderLineDto -> {
+            int num = finalPendingInventory ? 1 : 0;
+            beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity() - num);
         });
 
         jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
                 AllocateOrderResult.builder()
-                .beerOrderDto(request.getBeerOrderDto())
-                .pendingInventory(false)
-                .allocationError(false)
+                .beerOrderDto(beerOrderDto)
+                .pendingInventory(pendingInventory)
+                .allocationError(allocationError)
                 .build());
     }
 }
